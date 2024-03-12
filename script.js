216cd8920a84ab60a6e7cf4a00a15990edfc7c18
kubais <30256909+kubais@users.noreply.github.com>
2024-03-12 17:23:19 +0100

    Add eventListener to add class to div with mouseOver.

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 const container = document.querySelector('#container');
 
 function addDiv() {
-    for (let i=1; i<256; i++) {
+    for (let i=1; i<=256; i++) {
         
         const newDiv = document.createElement('div');
         newDiv.textContent = "x";
         newDiv.setAttribute("class", "newDiv");
         container.appendChild(newDiv);
-        
-        
-    }
- }
 
- addDiv();
+        newDiv.addEventListener('mouseover', e => e.target.classList.add('my-color-class') );
+    }
+}
+addDiv();
+
+// const divs = document.querySelectorAll('.newDiv');
+
+// divs.forEach((newDiv) => {
+//     newDiv.addEventListener('mouseover', () => {
+//         document.getElementsByClassName("newDiv")[1].style.color = "red";
+//     }
+//     )
+// }
+// );
+
+
+// divs.forEach((newDiv) => {
+//     newDiv.addEventListener('mouseout', () => {
+//         document.getElementsByClassName("newDiv")[1].style.color = "blue";
+//     }
+//     )
+// }
+// )
+
+
+
+
