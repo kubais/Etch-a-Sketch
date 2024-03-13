5e57527304ca4ae8bb6b3a7d1c8c1756ccd9f99b
kubais <30256909+kubais@users.noreply.github.com>
2024-03-13 16:01:25 +0100

    Change styles of the grid. Still not flexing.

--- script.js
+++ script.js
@@ -13,12 +13,45 @@ function addDiv() {
 }
 addDiv();
 
-button = document.querySelector('button');
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 let rows;
+
+function addDiv2() {
+    for (let i=1; i<=rows; i++) {
+    
+        const newDiv = document.createElement('div');
+        newDiv.textContent = "x";
+        newDiv.setAttribute("class", "newDiv");
+        container.appendChild(newDiv);
+
+        newDiv.addEventListener('mouseover', e => e.target.classList.add('my-color-class') );
+    }
+}
+
+button = document.querySelector('button');
 button.addEventListener('click', () => {
     rows = prompt('Enter desired number of squares per side (max 100):', 16);
+    rows = rows * rows
+    console.log(rows);
+
+    removeAllChildNodes(container)
+
+    addDiv2();
     
 })
+
+
+
+
+
+
+
+
 
 
 
