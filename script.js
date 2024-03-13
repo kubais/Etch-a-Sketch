5ee637d016c3f438abeb843ac8c9ec5c3ca32c9c
kubais <30256909+kubais@users.noreply.github.com>
2024-03-13 16:32:09 +0100

    Change default rows to 17.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const container = document.querySelector('#container');
 
-function addDiv() {
-    for (let i=1; i<=256; i++) {
+function addDivStart() {
+    for (let i=1; i<=289; i++) {
         
         const newDiv = document.createElement('div');
-        newDiv.textContent = "x";
+        // newDiv.textContent = "x";
         newDiv.setAttribute("class", "newDiv");
         container.appendChild(newDiv);
 
         newDiv.addEventListener('mouseover', e => e.target.classList.add('my-color-class') );
     }
 }
-addDiv();
+addDivStart();
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -21,11 +21,11 @@ function removeAllChildNodes(parent) {
 
 let rows;
 
-function addDiv2() {
+function addDivSettings() {
     for (let i=1; i<=rows; i++) {
     
         const newDiv = document.createElement('div');
-        newDiv.textContent = "x";
+        // newDiv.textContent = "x";
         newDiv.setAttribute("class", "newDiv");
         container.appendChild(newDiv);
 
@@ -35,13 +35,13 @@ function addDiv2() {
 
 button = document.querySelector('button');
 button.addEventListener('click', () => {
-    rows = prompt('Enter desired number of squares per side (max 100):', 16);
+    rows = prompt('Enter desired number of squares per side (max 100):', 17);
     rows = rows * rows
     console.log(rows);
 
     removeAllChildNodes(container)
 
-    addDiv2();
+    addDivSettings();
     
 })
 
