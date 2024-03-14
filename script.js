1005c5d5c0eaa6b0cdc1ed7b8c6aa8ef5acae8b7
kubais <30256909+kubais@users.noreply.github.com>
2024-03-14 10:05:19 +0100

    Add conditions for user prompt to accept only 1-100.

--- script.js
+++ script.js
@@ -19,7 +19,13 @@ function removeAllChildNodes(parent) {
     }
 }
 
-let rows;
+
+
+function randomColor (colorHex) {
+	colorHex =  "#" + (Math.random()	* 0xFFFFFF<<0).toString(16);
+    console.log(colorHex);
+    return colorHex;
+}
 
 function addDivSettings() {
     for (let i=1; i<=rows; i++) {
@@ -29,19 +35,36 @@ function addDivSettings() {
         newDiv.setAttribute("class", "newDiv");
         container.appendChild(newDiv);
 
-        newDiv.addEventListener('mouseover', e => e.target.classList.add('my-color-class') );
+        // newDiv.addEventListener('mouseover', e => e.target.style.backgroundColor = '${randomColor()}'); //to have random color
+        newDiv.addEventListener('mouseover', e => e.target.classList.add('my-color-class') ); // to change only to red
+        
     }
+}
+let rows;
+
+function playerSize () {
+    rows = parseInt(prompt('Enter desired number of squares per side (max 100):', 17));
+    if (rows < 1 || rows > 100) {
+        alert("Please enter number between 1 and 100");
+        playerSize();
+        }
+    else if (isNaN(rows)) {
+        alert("Please enter a number");
+        playerSize();
+    }
+    else {
+        rows = rows * rows
+        addDivSettings();
+    };
+    
 }
 
 button = document.querySelector('button');
 button.addEventListener('click', () => {
-    rows = prompt('Enter desired number of squares per side (max 100):', 17);
-    rows = rows * rows
-    console.log(rows);
-
+    
     removeAllChildNodes(container)
-
-    addDivSettings();
+    playerSize();     
+    
     
 })
 
